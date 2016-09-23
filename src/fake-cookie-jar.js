bf426d749f33359d52f7b391a8edfc47180ff81c
Oliver Spindler <os@oliverspindler.com>
2016-09-23 11:59:58 +0200

    Refactor file layout

---
+++ src/fake-cookie-jar.js
@@ -0,0 +1,21 @@
+function FakeCookieJar(cookies) {
+  var parsed = {};
+  Object.keys(cookies).forEach(function(key) {
+    parsed[unescape(key)] = cookies[key];
+  })
+  this.cookies = parsed;
+}
+
+FakeCookieJar.prototype.get = function (key) {
+  return this.cookies[key] || null;
+}
+
+FakeCookieJar.prototype.set = function (key, value) {
+  this.cookies[key] = value;
+}
+
+FakeCookieJar.prototype.expire = function (key) {
+  delete this.cookies[key];
+}
+
+module.exports = FakeCookieJar;
