e8b43a2097ad7e0a16555ddd3d14febefb909df9
Oliver Spindler <os@oliverspindler.com>
2017-12-21 16:38:41 +0100

    Add wrapper for node.js cookies library

---
+++ src/node-cookies-wrapper.js
@@ -0,0 +1,34 @@
+/*
+Wraps instance of Node.js cookie implementation [1] to match API of Cookies.js
+browser library [2].
+
+[1] https://github.com/pillarjs/cookies
+[2] https://github.com/ScottHamper/Cookies
+*/
+
+function NodeCookiesWrapper(cookies) {
+  this.cookies = cookies;
+}
+
+NodeCookiesWrapper.prototype.get = function (key) {
+  key = encodeKey(key);
+  return decodeURIComponent(this.cookies.get(key));
+}
+
+NodeCookiesWrapper.prototype.set = function (key, value, options) {
+  key = encodeKey(key);
+  value = (value + '').replace(/[^!#$&-+\--:<-\[\]-~]/g, encodeURIComponent);
+  this.cookies.set(key, value, options);
+}
+
+NodeCookiesWrapper.prototype.expire = function (key) {
+  this.set(key, undefined);
+}
+
+function encodeKey (key) {
+  key = key.replace(/[^#$&+\^`|]/g, encodeURIComponent);
+  key = key.replace(/\(/g, '%28').replace(/\)/g, '%29');
+  return key;
+}
+
+module.exports = NodeCookiesWrapper;
